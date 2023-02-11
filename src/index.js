44438c9345d3d7895c91aaa925cad0273361d9e3
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-02-11 14:16:33 -0800

    working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
 import c3 from "c3";
 
-const chart = c3.generate({
-    bindto: '#chart',
-    data: {
-      columns: [
-        ['data1', 30, 200, 100, 400, 150, 250],
-        ['data2', 50, 20, 10, 40, 15, 25]
-      ]
+const data =[
+    {
+    "month": "Jan",
+    "Apples": 327,
+    "Peaches": 437,
+    "Pears": 111
+    },
+    {
+    "month": "Feb",
+    "Apples": 555,
+    "Peaches": 454,
+    "Pears": 222
+    },
+    {
+    "month": "Mar",
+    "Apples": 242,
+    "Peaches": 864,
+    "Pears": 343
+    },
+    {
+    "month": "Apr",
+    "Apples": 222,
+    "Peaches": 454,
+    "Pears": 434
     }
-});
+];
+
+const options = {
+    bindto: '#chart',
+    axis:{
+        x: { type: "category"},
+        y: {},
+    },
+    data: {
+        labels: true,
+        type: "bar",
+        json: data,
+        keys: {
+            x: "month",
+            value: ["Apples", "Peaches", "Pears"],
+        },
+    },
+};
+
+const chart = c3.generate(options)
